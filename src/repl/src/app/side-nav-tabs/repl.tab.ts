7f87b3558664e63db98fa1245804dafcac18b0c2
youwol-guillaume <greinisch@youwol.com>
2023-03-01 18:27:23 +0100

    ✨ repl.repl-tab => provide left green border helper for cell

--- src/repl/src/app/side-nav-tabs/repl.tab.ts
+++ src/repl/src/app/side-nav-tabs/repl.tab.ts
@@ -2,6 +2,8 @@ import { DockableTabs } from '@youwol/fv-tabs'
 import { Common } from '@youwol/fv-code-mirror-editors'
 import { attr$, childrenFromStore$, VirtualDOM } from '@youwol/flux-view'
 import { AppState } from '../app.state'
+import { map } from 'rxjs/operators'
+import { combineLatest } from 'rxjs'
 /**
  * @category View
  */
@@ -82,14 +84,30 @@ export class ReplView implements VirtualDOM {
                 },
             },
         })
+        const isLast$ = combineLatest([
+            this.state.cells$,
+            this.state.projectByCells$,
+        ]).pipe(
+            map(([cells, projectByCells]) => {
+                const index = cells.indexOf(this.ideState)
+                return projectByCells.has(cells[index + 1])
+            }),
+        )
         this.children = [
             {
                 style: attr$(this.state.projectByCells$, (hist) => {
                     return hist.has(this.ideState)
-                        ? { opacity: 1 }
+                        ? { opacity: 1, borderWidth: '5px' }
                         : { opacity: 0.5 }
                 }),
-                class: 'w-100 h-100',
+                class: attr$(
+                    isLast$,
+                    (isLast): string =>
+                        isLast
+                            ? 'fv-border-left-success border-3'
+                            : 'w-100 h-100',
+                    { wrapper: (d) => `${d} w-100 h-100` },
+                ),
                 children: [ideView],
             },
         ]
