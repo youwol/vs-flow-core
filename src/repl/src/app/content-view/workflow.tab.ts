5150e1aa0bba66e1728526907d699ddb2d1bbdaf
youwol-guillaume <greinisch@youwol.com>
2023-03-02 11:12:08 +0100

    ♻️ repl.workflow => exposed environment3D in repl

--- src/repl/src/app/content-view/workflow.tab.ts
+++ src/repl/src/app/content-view/workflow.tab.ts
@@ -14,11 +14,13 @@ export class WorkflowTab implements VirtualDOM {
     constructor(params: { state: AppState; project: ProjectState }) {
         Object.assign(this, params)
         this.style = this.project ? { opacity: 1 } : { opacity: 0.3 }
-        this.children = [
-            new Renderer3DView({
-                project: this.project || this.state.lastAvailableProject,
-                uidSelected: this.state.selectedUid$,
-            }),
-        ]
+        const renderer3d = new Renderer3DView({
+            project: this.project || this.state.lastAvailableProject,
+            uidSelected: this.state.selectedUid$,
+        })
+        renderer3d.environment3D$.subscribe((env3d) => {
+            this.state.repl.attributes['env3d'] = env3d
+        })
+        this.children = [renderer3d]
     }
 }
